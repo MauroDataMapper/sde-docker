d98e78162192ee81c130f70f89f2c4e6a30aeb6e
James Welch <james.welch@bdi.ox.ac.uk>
2024-10-16 12:58:54 +0100

    Allow for pre-built versions of explorer and sde-admin to be dropped in

--- mauro-data-mapper/build_scripts/build_explorer.sh
+++ mauro-data-mapper/build_scripts/build_explorer.sh
@@ -2,27 +2,37 @@
 
 precompiledBuild(){
    echo 'Using pre-compiled source'
+   
+  if [ -n "$( ls -A '/prebuilt-explorer' )" ]; then
+    echo "A pre-existing build for Mauro Explorer has been provided - using that instead of downloading a release"
+    cp -r /prebuilt-explorer/* "$MDM_EXPLORER_BUILD_HOME"
+  
+  else
+   echo "Not using a pre-existing build for Mauro Explorer - downloading a release..."
 
-  if [[ $MDM_EXPLORER_VERSION == *SNAPSHOT ]]
-  then
-    MDM_EXPLORER_LIBRARY='artifacts-snapshots'
-   else
-     MDM_EXPLORER_LIBRARY='artifacts'
-   fi
+   if [[ $MDM_EXPLORER_VERSION == *SNAPSHOT ]]
+     then
+       MDM_EXPLORER_LIBRARY='artifacts-snapshots'
+     else
+       MDM_EXPLORER_LIBRARY='artifacts'
+     fi
 
-  MDM_EXPLORER_URL="https://mauro-repository.com/${MDM_EXPLORER_LIBRARY}/mauroDataMapper/mdm-explorer/mdm-explorer-${MDM_EXPLORER_VERSION}.tgz"
+    MDM_EXPLORER_URL="https://mauro-repository.com/${MDM_EXPLORER_LIBRARY}/mauroDataMapper/mdm-explorer/mdm-explorer-${MDM_EXPLORER_VERSION}.tgz"
+
+    echo "Downloading precompiled sources ${MDM_EXPLORER_URL}"
+
+    cd /opt || exit
+    curl -LO "$MDM_EXPLORER_URL"
+    tar xzf "mdm-explorer-${MDM_EXPLORER_VERSION}.tgz"
+    ls
+    mkdir "$MDM_EXPLORER_BUILD_HOME"
+    cp -r "mdm-explorer-${MDM_EXPLORER_VERSION}"/* "$MDM_EXPLORER_BUILD_HOME"
+
+  fi
+
   SDE_RESEARCHER_API_ENDPOINT="http://localhost:8094/"
 
-  echo "Downloading precompiled sources ${MDM_EXPLORER_URL}"
-
-  cd /opt || exit
-  curl -LO "$MDM_EXPLORER_URL"
-  tar xzf "mdm-explorer-${MDM_EXPLORER_VERSION}.tgz"
-  ls
-  mkdir "$MDM_EXPLORER_BUILD_HOME"
-  cp -r "mdm-explorer-${MDM_EXPLORER_VERSION}"/* "$MDM_EXPLORER_BUILD_HOME"
-
-  find "$MDM_EXPLORER_BUILD_HOME" -name main.*.js -exec sed -e "s|apiEndpoint:\"api\",|apiEndpoint:\"${MDM_EXPLORER_API_ENDPOINT}\",|g" -i {} \;
+  find "$MDM_EXPLORER_BUILD_HOME" -name main.*.js -exec sed -e "s|apiEndpoint:\"api\"|apiEndpoint:\"${MDM_EXPLORER_API_ENDPOINT}\"|g" -i {} \;
   find "$MDM_EXPLORER_BUILD_HOME" -name main.*.js -exec sed -e "s|baseUrl:undefined|baseUrl:\"${SDE_RESEARCHER_API_ENDPOINT}\"|g" -i {} \;
 
 }
--- mauro-data-mapper/build_scripts/build_sde_admin.sh
+++ mauro-data-mapper/build_scripts/build_sde_admin.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
 precompiledBuild(){
-   echo 'Using pre-compiled source'
+    echo 'Using pre-compiled source'
+    echo SDE ADMIN BUILD HOME $SDE_ADMIN_BUILD_HOME
 
-  if [[ $SDE_ADMIN_VERSION == *SNAPSHOT ]]
-  then
-    SDE_ADMIN_LIBRARY='artifacts-snapshots'
-   else
-    SDE_ADMIN_LIBRARY='artifacts'
-   fi
+    SDE_ADMIN_API_ENDPOINT="http://localhost:8093/"
 
-  SDE_ADMIN_URL="https://mauro-repository.com/${SDE_ADMIN_LIBRARY}/mauroDataMapper/sde-admin-ui/sde-admin-ui-${SDE_ADMIN_VERSION}.tgz"
-  SDE_RESEARCHER_API_ENDPOINT="http://localhost:8093/"
+  if [ -n "$( ls -A '/prebuilt-sde-admin' )" ]; then
+    echo "A pre-existing build for SDE Admin System has been provided - using that instead of downloading a release"
+    cp -r /prebuilt-sde-admin/* "$SDE_ADMIN_BUILD_HOME"
 
-  echo "Downloading precompiled sources ${SDE_ADMIN_URL}"
+    ls $SDE_ADMIN_BUILD_HOME
 
-  cd /opt || exit
-  curl -LO "$SDE_ADMIN_URL"
-  tar xzf "sde-admin-ui-${SDE_ADMIN_VERSION}.tgz"
-  
-  mkdir $SDE_ADMIN_BUILD_HOME
-  cp -r sde-admin-ui-${SDE_ADMIN_VERSION}/* $SDE_ADMIN_BUILD_HOME
+    grep 'baseUrl:""' $SDE_ADMIN_BUILD_HOME/*
+    grep 'baseUrl:"api"' $SDE_ADMIN_BUILD_HOME/*
+    
+  else
+    echo "Not using a pre-existing build for SDE Admin System - downloading a release..."
 
-  ls $SDE_ADMIN_BUILD_HOME
-  
+    if [[ $SDE_ADMIN_VERSION == *SNAPSHOT ]]
+     then
+      SDE_ADMIN_LIBRARY='artifacts-snapshots'
+     else
+      SDE_ADMIN_LIBRARY='artifacts'
+    fi
+
+    SDE_ADMIN_URL="https://mauro-repository.com/${SDE_ADMIN_LIBRARY}/mauroDataMapper/sde-admin-ui/sde-admin-ui-${SDE_ADMIN_VERSION}.tgz"
+
+    echo "Downloading precompiled sources ${SDE_ADMIN_URL}"
+
+    cd /opt || exit
+    curl -LO "$SDE_ADMIN_URL"
+    tar xzf "sde-admin-ui-${SDE_ADMIN_VERSION}.tgz"
+   
+    mkdir $SDE_ADMIN_BUILD_HOME
+    cp -r sde-admin-ui-${SDE_ADMIN_VERSION}/* $SDE_ADMIN_BUILD_HOME
+
+  fi
+   
 #  find "$MDM_EXPLORER_BUILD_HOME" -name main.*.js -exec sed -e "s|apiEndpoint:\"api\",|apiEndpoint:\"${MDM_EXPLORER_API_ENDPOINT}\",|g" -i {} \;
-  find "$SDE_ADMIN_BUILD_HOME" -name main.*.js -exec sed -e "s|baseUrl:\"api\"|baseUrl:\"${SDE_RESEARCHER_API_ENDPOINT}\"|g" -i {} \;
+   find "$SDE_ADMIN_BUILD_HOME" -name main.*.js -exec sed -e "s|baseUrl:\"api\"|baseUrl:\"${SDE_ADMIN_API_ENDPOINT}\"|g" -i {} \;
 
 }
 
